09e3436beb486cd154a02762b79fac853d806d3e
Mike Lambert <cyberspacelabs@gmail.com>
2020-06-06 12:53:40 +0300

    fixes a bit

--- killswitch-common/src/main/java/com/github/mikelambert/killswitch/event/KillswitchAdminStatus.java
+++ killswitch-common/src/main/java/com/github/mikelambert/killswitch/event/KillswitchAdminStatus.java
@@ -1,8 +1,10 @@
 package com.github.mikelambert.killswitch.event;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class KillswitchAdminStatus extends BooleanState {
     public KillswitchAdminStatus(boolean value){
         this.value = value;
--- killswitch-common/src/main/java/com/github/mikelambert/killswitch/event/KillswitchArmedStatus.java
+++ killswitch-common/src/main/java/com/github/mikelambert/killswitch/event/KillswitchArmedStatus.java
@@ -1,8 +1,10 @@
 package com.github.mikelambert.killswitch.event;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class KillswitchArmedStatus extends BooleanState {
     public KillswitchArmedStatus(boolean value){
         this.value = value;
