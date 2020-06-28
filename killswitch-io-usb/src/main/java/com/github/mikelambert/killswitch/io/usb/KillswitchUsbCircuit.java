4b11da910f8757226597ed853706c6888462caf0
Mike Lambert <cyberspacelabs@gmail.com>
2020-06-28 11:11:17 +0300

    USB circuit

--- killswitch-io-usb/src/main/java/com/github/mikelambert/killswitch/io/usb/KillswitchUsbCircuit.java
+++ killswitch-io-usb/src/main/java/com/github/mikelambert/killswitch/io/usb/KillswitchUsbCircuit.java
@@ -1,12 +1,23 @@
 package com.github.mikelambert.killswitch.io.usb;
 
 import android.content.Context;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 
 import com.github.mikelambert.killswitch.common.CircuitState;
 import com.github.mikelambert.killswitch.common.HardwareCircuit;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
 
 public class KillswitchUsbCircuit implements HardwareCircuit {
+    private final Context context;
+    private final String descriptor;
+    private UsbDevice device;
+
     public KillswitchUsbCircuit(Context context, String descriptor) {
+        this.context = context;
+        this.descriptor = descriptor;
     }
 
     @Override
@@ -57,5 +68,23 @@ public class KillswitchUsbCircuit implements HardwareCircuit {
     @Override
     public String getDescriptor() {
         return null;
+    }
+
+    private void refresh() {
+        UsbManager usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+        UsbSerialProber usbDefaultProber = UsbSerialProber.getDefaultProber();
+        for(UsbDevice device : usbManager.getDeviceList().values()) {
+            UsbSerialDriver driver = usbDefaultProber.probeDevice(device);
+            if(driver != null) {
+                for(int port = 0; port < driver.getPorts().size(); port++){
+                    UsbSerialPort p = driver.getPorts().get(port);
+                    //TODO: match
+                    //p.getDevice().
+                }
+
+            } else {
+
+            }
+        }
     }
 }
