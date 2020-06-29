fd6d13e360ab6a35c64e592f86234ef5d65f7907
Mike Lambert <cyberspacelabs@gmail.com>
2020-06-29 13:58:44 +0300

    SMS receiver

---
+++ killswitch-common/src/main/java/com/github/mikelambert/killswitch/sms/SmsReceiver.java
@@ -0,0 +1,46 @@
+package com.github.mikelambert.killswitch.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+    private static final String TAG =
+            SmsReceiver.class.getSimpleName();
+    public static final String pdu_type = "pdus";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Get the SMS message.
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs;
+        String strMessage = "";
+        String format = bundle.getString("format");
+        // Retrieve the SMS message received.
+        Object[] pdus = (Object[]) bundle.get(pdu_type);
+        if (pdus != null) {
+            // Fill the msgs array.
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+                // Check Android version and use appropriate createFromPdu.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    // If Android version M or newer:
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    // If Android version L or older:
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                // Build the message to show.
+                strMessage += msgs[i].getOriginatingAddress();
+                strMessage += " :" + msgs[i].getMessageBody() + "\n";
+                // Log and display the SMS message.
+                Log.d(TAG, "onReceive: " + strMessage);
+                // TODO: pass to Killswitch
+            }
+        }
+    }
+}
